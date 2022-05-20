223e8aba172b46521e0a9e1ad46008e3aa1d3811
hilit <hilitaharoni@gmail.co>
2022-05-20 16:08:22 +0300

    skeleton creation is now fast

--- pose_estimator/skeleton_creator.py
+++ pose_estimator/skeleton_creator.py
@@ -1,51 +1,44 @@
 import pandas as pd
 from pose_estimator.consts import BODYPARTS, BONES
-import matplotlib.pyplot as plt
-from matplotlib.collections import LineCollection
-import matplotlib.patches as patches
-
-def plot_bone(xs, ys, bone):
-    plt.plot(xs.loc[bone], ys.loc[bone], linewidth=2, color='b')
+import cv2
 
 
-def plot_skeleton(xs, ys):
+def plot_bone(bg_img, xs, ys, bone):
+    assert len(bone) == 2
+    start_point = (xs.loc[bone[0]], ys.loc[bone[0]])
+    end_point = (xs.loc[bone[1]], ys.loc[bone[1]])
+    cv2.line(bg_img, start_point, end_point, color=(0, 255, 0), thickness=2)
+
+
+def plot_skeleton(bg_img, xs, ys):
+    xs, ys = xs.apply(round), ys.apply(round)
     # plot nodes
-    plt.scatter(xs, ys, s=6)
+    for center_coordinates in zip(xs, ys):
+        cv2.circle(bg_img, center_coordinates, radius=1, color=(255, 0, 0), thickness=3)
 
-    # plot bones
     for bone in BONES:
-        plot_bone(xs, ys, list(bone))
+        plot_bone(bg_img, xs, ys, list(bone))
 
-    plt.gca().invert_yaxis()
+    cv2.imwrite('test.png', bg_img)
 
 
 def generate_images(fixed_pose_df, mode='sticklight', background_image=None):
     final_images_list = []
-    
+
     # load background_image
     height, width, channel = background_image.shape
-    aspect_ratio = float(width) / height
-    fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
-    # To remove the huge white borders
-    fig.tight_layout(pad=0)
-    ax.margins(0)
-    ax.set_yticklabels([])
-    ax.set_xticklabels([])
-    plt.axis('off')
-
-    im = ax.imshow(background_image)
 
     for i, v in fixed_pose_df.iterrows():
         xs, ys, scores = v.apply(lambda x: x[1]), v.apply(lambda x: x[0]), v.apply(lambda x: x[2])
 
         # change ratios of the skeleton to fit image shape
-        xs *= background_image.shape[1]
-        ys *= background_image.shape[0]
+        xs *= width
+        ys *= height
 
         # TODO: filter high scores only
 
         # add skeleton
-        plot_skeleton(xs, ys)
+        plot_skeleton(background_image, xs, ys)
 
         # TODO: add emoji head
 
