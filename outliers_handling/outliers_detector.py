6d0f74e1c3f8c75e5dc53baa35b8fcb0dd7702db
hilit <hilitaharoni@gmail.co>
2022-05-27 18:05:17 +0300

    added smoothing for the head size

--- outliers_handling/outliers_detector.py
+++ outliers_handling/outliers_detector.py
@@ -2,7 +2,11 @@ def detect_outliers(pose_df):
     # TODO: Detect outliers, return list of anomaly_inds
     return []
 
+
 def fix_outliers(pose_df, anomaly_inds):
     # TODO: Fix outliers
-
+    if len(anomaly_inds) == 0:
+        # smooth
+        pose_df[['face_width', 'face_height']] = pose_df[['face_width', 'face_height']].rolling(50).mean().fillna(
+            method='bfill')
     return pose_df
